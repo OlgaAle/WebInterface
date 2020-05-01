137a2b14ef3fb9e5c5a47e5f6fae3dbfba31ffef
olga Aleksandrova <olgan1983@yandex.ru>
2020-05-01 23:44:03 +0300

    Form test minor fix.

--- src/test/java/FormTest.java
+++ src/test/java/FormTest.java
@@ -1,10 +1,5 @@
 import com.codeborne.selenide.SelenideElement;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 
 import static com.codeborne.selenide.Condition.exactText;
 import static com.codeborne.selenide.Selenide.$;
